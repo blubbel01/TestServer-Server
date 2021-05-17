031c3f7a0466eab9828a8a3f11e49f3730c4d150
blubbel <paul@jippa.de>
2021-05-17 19:14:39 +0200

    fix health & armour

--- listeners/PlayerDeathListener.js
+++ listeners/PlayerDeathListener.js
@@ -8,5 +8,7 @@ mp.events.add("playerDeath", (player, reason, killer) => {
     setTimeout(() => {
         player.spawn(new mp.Vector3(-425.517, 1123.620, 325.8544));
         player.dimension = 0;
+        player.armour = 100;
+        player.health = 100;
     }, 500);
 });
