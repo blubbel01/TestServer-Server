6a996db563d898fe0ccb909cab7e2b9c84cf20b1
blubbel <paul@jippa.de>
2021-05-18 15:30:53 +0200

    update database

--- database/config.js
+++ database/config.js
@@ -4,8 +4,8 @@ module.exports = {
     database: {
         username: "test-server",
         password: "D_^Yn8tDWaz7CWr4FsrAG=3t%?QL^nRT",
-        database: "test-server",
-        host: "127.0.0.1",
+        database: "test_server",
+        host: "141.95.0.57",
         port: 3306,
         dialect: "mysql",
         operatorsAliases: false,
