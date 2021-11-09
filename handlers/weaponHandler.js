c8f5afb13db58c352d7117eaad28bcbda9e39f05
blubbel <paul@jippa.de>
2021-11-09 21:30:18 +0100

    Update damage values

--- handlers/weaponHandler.js
+++ handlers/weaponHandler.js
@@ -37,39 +37,48 @@ class WeaponHandler {
         WeaponHandler.weaponData.set(0xC1B3C3D1, { // weapon_revolver
             damageBase: 108,
             range: 200,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0xCB96392F, { // weapon_revolver_mk2
             damageBase: 100,
             range: 200,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0x97EA20B8, { // weapon_doubleaction
             damageBase: 75,
             range: 200,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0x99AEEB3B, { // weapon_pistol50
             damageBase: 51,
             range: 200,
+            shotgun: false,
         });
 
         WeaponHandler.weaponData.set(0x2BE6766B, { // weapon_smg
             damageBase: 18,
             range: 150,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0x0A3D4D34, { // weapon_combatpdw
             damageBase: 21,
             range: 150,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0xDB1AA450, { // weapon_machinepistol
             damageBase: 24,
             range: 150,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0xEFE7E2DF, { // weapon_assaultsmg
             damageBase: 20,
             range: 150,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0x13532244, { // weapon_microsmg
             damageBase: 18,
             range: 150,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0x1D073A89, { // weapon_pumpshotgun
             damageBase: 19,
@@ -80,45 +89,60 @@ class WeaponHandler {
         WeaponHandler.weaponData.set(0xBFEFFF6D, { // weapon_assaultrifle
             damageBase: 38,
             range: 150,
-            shotgun: true,
+            shotgun: false,
+        });
+        WeaponHandler.weaponData.set(0x84D6FAFD, { // weapon_bullpuprifle_mk2
+            damageBase: 33,
+            range: 150,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0x83BF0278, { // weapon_carbinerifle
             damageBase: 25,
             range: 150,
-            shotgun: true,
+            shotgun: false,
+        });
+        WeaponHandler.weaponData.set(0xFAD1F1C9, { // weapon_carbinerifle_mk2
+            damageBase: 25,
+            range: 150,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0xAF113F99, { // weapon_advancedrifle
             damageBase: 30,
             range: 150,
-            shotgun: true,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0x624FE830, { // weapon_compactrifle
             damageBase: 40,
             range: 150,
-            shotgun: true,
+            shotgun: false,
         });
 
         WeaponHandler.weaponData.set(0xDBBD7280, { // weapon_combatmg_mk2
             damageBase: 30,
             range: 100,
-            shotgun: true,
+            shotgun: false,
         });
         WeaponHandler.weaponData.set(0x61012683, { // weapon_gusenberg
             damageBase: 24,
             range: 150,
-            shotgun: true,
+            shotgun: false,
         });
 
         WeaponHandler.weaponData.set(0x05FC3C11, { // weapon_sniperrifle
             damageBase: 308,
             range: 2000,
-            shotgun: true,
+            shotgun: false,
         });
 
         WeaponHandler.weaponData.set(0xDC4DB296, { // weapon_marksmanpistol
             damageBase: 220,
             range: 500,
-            shotgun: true,
+            shotgun: false,
+        });
+        WeaponHandler.weaponData.set(0xA89CB99E, { // weapon_musket
+            damageBase: 65,
+            range: 500,
+            shotgun: false,
         });
     }
 
